ac8aed3967aa59142741d9480264274271115f29
leonwesteveld <leonwesterveld12@gmail.com>
2026-03-23 15:44:31 +0100

    start of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
-import './Navbar.css'
+import './Navbar.css';
 
 function Navbar() {
-
   return (
-    <>
-    <h1>Navbar component</h1>
-    </>
-  )
+    <nav className="nav">
+      <div className="nav__track">
+        <ul className="nav__list">
+          <li><a className="nav__link" href="#home">NEBULA-Xplorer</a></li>
+          <li><a className="nav__link" href="#Cosine">Cosine</a></li>
+          <li><a className="nav__link" href="#Airbus">Airbus</a></li>
+          <li><a className="nav__link" href="#Technolution">Technolution</a></li>
+          <li><a className="nav__link" href="#Dawn">Dawn Aerospace</a></li>
+          <li><a className="nav__link" href="#Nlr">nlr</a></li>
+          <li><a className="nav__link" href="#Spherical">Spherical</a></li>
+          <li><a className="nav__link" href="#Airborne">Airborne</a></li>
+          <li><a className="nav__link" href="#IsisSpace">IsisSpace</a></li>
+          <li><a className="nav__link" href="#Starion">Starion</a></li>
+          <li><a className="nav__link" href="#Celestia">Celestia</a></li>
+          <li><a className="nav__link" href="#Vdl">Vdl Etg</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
